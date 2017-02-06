aa2636d471a52ebb7e6a118b0f3bff584f8d3844
Alexey Demin <deminn@gmail.com>
2017-02-06 00:01:36 -0300

    Show basic info

---
+++ resources/views/data.blade.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+              <table border="1">
+                  <tr><td>Name</td><td>{{ $data['name'] }} </td></tr>
+                  <tr><td>Birthday</td><td>{{ $data['birthday'] }} </td></tr>
+                  <tr><td>Devices</td>
+                      <td>
+                          @forelse ($data['devices'] as $item)
+                              <li>{{ $item['os'] }}</li>
+                          @empty
+                              <p>No items</p>
+                          @endforelse
+                      </td>
+                  </tr>
+                  <tr><td>Education</td>
+                      <td>
+                          @forelse ($data['education'] as $item)
+                              <li>{{ $item['school']['name'] }} {{ $item['year']['name'] }}</li>
+                          @empty
+                              <p>No items</p>
+                          @endforelse
+                      </td>
+                  </tr>
+                  <tr><td>Email</td><td>{{ $data['email'] }} </td></tr>
+                  <tr><td>Gender</td><td>{{ $data['gender'] }} </td></tr>
+                  <tr><td>Hometown</td><td>{{ $data['hometown']['name'] }} </td></tr>
+                  <tr><td>Languages</td>
+                      <td>
+                          @forelse ($data['languages'] as $item)
+                              <li>{{ $item['name'] }}</li>
+                          @empty
+                              <p>No items</p>
+                          @endforelse
+                      </td>
+                  </tr>
+                  <tr><td>Location</td><td>{{ $data['location']['name'] }} </td></tr>
+              </table>
+<img src="{{ $data['cover']['source'] }}">
+
+            </div>
+        </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,195 @@
 |
 */
 
+
+$permission_array = [
+    'public_profile',
+    'user_friends',
+    'email',
+    'user_about_me',
+    'user_actions.books',
+    'user_actions.fitness',
+    'user_actions.music',
+    'user_actions.news',
+    'user_actions.video',
+    'user_birthday',
+    'user_education_history',
+    'user_events',
+    'user_games_activity',
+    'user_hometown',
+    'user_likes',
+    'user_location',
+    'user_managed_groups',
+    'user_photos',
+    'user_posts',
+    'user_relationships',
+    'user_relationship_details',
+    'user_religion_politics',
+    'user_tagged_places',
+    'user_videos',
+    'user_website',
+    'user_work_history',
+    'read_custom_friendlists',
+    'read_insights',
+    'read_audience_network_insights',
+    'read_page_mailboxes',
+    'manage_pages',
+    'publish_pages',
+    'publish_actions',
+    'rsvp_event',
+    'pages_show_list',
+    'pages_manage_cta',
+    'pages_manage_instant_articles',
+    'ads_read',
+    'ads_management',
+    'business_management',
+    'pages_messaging',
+    'pages_messaging_subscriptions',
+    'pages_messaging_phone_number'
+];
+
+$fields_array =
+    [
+        'id'
+        ,'about'
+        //,'admin_notes'
+        ,'age_range'
+        ,'birthday'
+        ,'context'
+        ,'cover'
+        ,'currency'
+        ,'devices'
+        ,'education'
+        ,'email'
+        //,'employee_number'
+        ,'favorite_athletes'
+        ,'favorite_teams'
+        ,'first_name'
+        ,'gender'
+        ,'hometown'
+        ,'inspirational_people'
+        ,'install_type'
+        ,'installed'
+     //  ,'interested_in'
+       ,'is_shared_login'
+       ,'is_verified'
+     //  ,'labels'
+        ,'languages'
+        ,'last_name'
+        ,'link'
+        ,'locale'
+        ,'location'
+        ,'meeting_for'
+        ,'middle_name'
+        ,'name'
+        ,'name_format'
+        ,'payment_pricepoints'
+        ,'political'
+        ,'public_key'
+        ,'quotes'
+        ,'relationship_status'
+        ,'religion'
+        ,'security_settings'
+        ,'shared_login_upgrade_required_by'
+        ,'significant_other'
+        ,'sports'
+        ,'test_group'
+        ,'third_party_id'
+        ,'timezone'
+        ,'updated_time'
+        ,'verified'
+        ,'video_upload_limits'
+        ,'viewer_can_send_gift'
+        ,'website'
+        //,'work'
+];
 Route::get('/', function () {
-    return view('welcome');
+    return view('data');
+});
+
+Route::get('/facebook/login', function(SammyK\LaravelFacebookSdk\LaravelFacebookSdk $fb) use ($permission_array)
+{
+    // Send an array of permissions to request
+    $login_url = $fb->getLoginUrl($permission_array);
+
+    // Obviously you'd do this in blade :)
+    echo '<a href="' . $login_url . '">Login with Facebook</a>';
+});
+
+// Endpoint that is redirected to after an authentication attempt
+Route::get('/facebook/callback', function(SammyK\LaravelFacebookSdk\LaravelFacebookSdk $fb) use($fields_array)
+{
+    // Obtain an access token.
+    try {
+        $token = $fb->getAccessTokenFromRedirect();
+    } catch (Facebook\Exceptions\FacebookSDKException $e) {
+        dd($e->getMessage());
+    }
+
+    // Access token will be null if the user denied the request
+    // or if someone just hit this URL outside of the OAuth flow.
+    if (! $token) {
+        // Get the redirect helper
+        $helper = $fb->getRedirectLoginHelper();
+
+        if (! $helper->getError()) {
+            abort(403, 'Unauthorized action.');
+        }
+
+        // User denied the request
+        dd(
+            $helper->getError(),
+            $helper->getErrorCode(),
+            $helper->getErrorReason(),
+            $helper->getErrorDescription()
+        );
+    }
+
+    if (! $token->isLongLived()) {
+        // OAuth 2.0 client handler
+        $oauth_client = $fb->getOAuth2Client();
+
+        // Extend the access token.
+        try {
+            $token = $oauth_client->getLongLivedAccessToken($token);
+        } catch (Facebook\Exceptions\FacebookSDKException $e) {
+            dd($e->getMessage());
+        }
+    }
+
+    $fb->setDefaultAccessToken($token);
+
+    // Save for later
+    Session::put('fb_user_access_token', (string) $token);
+
+
+
+    $fields_string  = implode(',', $fields_array);
+
+    // Get basic info on the user from Facebook.
+    try {
+        $response = $fb->get('/me?fields=' . $fields_string, $token);
+    } catch (Facebook\Exceptions\FacebookSDKException $e) {
+        dd($e->getMessage());
+    }
+
+    $data = $response->getDecodedBody();
+
+    // Convert the response to a `Facebook/GraphNodes/GraphUser` collection
+    $facebook_user = $response->getGraphUser();
+
+    // Create the user if it does not exist or update the existing entry.
+    // This will only work if you've added the SyncableGraphNodeTrait to your User model.
+/*    $user = App\User::updateOrCreate([
+        'email' => $facebook_user->getEmail(),
+        'name' => $facebook_user->getFirstName() . ' '  . $facebook_user->getLastName(),
+        'password' => $facebook_user->getEmail()
+    ]);*/
+
+    // Log the user into Laravel
+    //Auth::login($user);
+
+
+return  view('data', compact('data'));
+//redirect('/')->with($response);
 });
